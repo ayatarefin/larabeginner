32df65d537b4544b3672fbe3e1c1f8cacd3f6105
ayatarefin <ayatullaharefin@gmail.com>
2023-10-11 12:36:53 +0600

    route practice

---
+++ resources/views/about.blade.php
@@ -0,0 +1,2 @@
+<h1> This is my About Page</h1>
+<h3><a href="{{url('/')}}"> back</a></h3>
--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// Route::get('/about', function () {
+//     return view('about');
 // });
+// this is another process
+Route::view('/about','about');
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+// Route parameter example
+
+Route::get('/search/{roll}', function($roll){
+    return ("his roll is $roll");
+});
+
+
+
+// just ignore this part now
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
